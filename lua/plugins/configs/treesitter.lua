a0f916c45c919e60c19dcd9e0fdbbc24887850c5
bousii <owen.a.giles@gmail.com>
2026-02-17 21:26:02 -0600

    Add more treesitter options

--- lua/plugins/configs/treesitter.lua
+++ lua/plugins/configs/treesitter.lua
@@ -1,6 +1,7 @@
 require("nvim-treesitter.configs").setup {
   ensure_installed = { "lua", "vim", "vimdoc", "c", "python", "cpp", "rust",
-                       "make", "meson", "kconfig", "perl", "bash" },
+                       "make", "meson", "kconfig", "perl", "bash",
+                       "gdscript", "godot_resource", "gdshader" },
 
   highlight = {
     enable = true,
